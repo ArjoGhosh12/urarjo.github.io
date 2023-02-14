a6166fd0002d6fc39e31fa8211a462f71724a480
Arjo Ghosh <122384138+ArjoGhosh12@users.noreply.github.com>
2023-02-14 23:56:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+alert("Are u interested to use my calculator")
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'sin'){
+      string =Math.sin(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'cos'){
+      string = Math.cos(string);
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
